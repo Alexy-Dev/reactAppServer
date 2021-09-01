3c92e75eadef87b7ef11259a77d62c4bd75e3db7
Alexy-Dev <alexsnow73@gmail.com>
2021-09-01 09:41:38 +0300

    ListItemWithHooks

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,49 +1,62 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import './itemList.css';
 import Spinner from '../spinner';
 
-export default class ItemList extends Component {
+// class ItemList extends Component {
+    function ItemList({getData, onItemSelected, renderItem}){
 
-    state = {
-        itemList: null
-    }
+        const [itemList, updateList] = useState([]);
 
-    componentDidMount() {
-        const {getData} = this.props;
-
-        getData()
-            .then( (itemList) => {
-                this.setState({
-                    itemList
+    // state = {
+    //     itemList: null
+    // }
+        useEffect(() => {
+            getData()
+            .then( (data) => {
+                updateList(data)
                 })
-            })
-    }
+            }, [])
+        
+    // componentDidMount() {
+    //     // const {getData} = this.props;
 
-    renderItems(arr) {
+    //     getData()
+    //         .then( (itemList) => {
+    //             this.setState({
+    //                 itemList
+    //             })
+    //         })
+    // }
+
+    function renderItems(arr) {
         return arr.map((item) => {
             const {id} = item;
 
-            const label = this.props.renderItem(item);
+            // const label = this.props.renderItem(item);
+            const label = renderItem(item);
 
             return (
                 <li 
                     key={id}
                     className="list-group-item"
-                    onClick={ () => this.props.onItemSelected(id)}>
+                    // onClick={ () => this.props.onItemSelected(id)}>
+                    onClick={ () => onItemSelected(id)}>
                     {label}
                 </li>
             )
         })
     }
 
-    render() {
-        const {itemList} = this.state;
+    // render() {
+        // const {itemList} = this.state;
 
         if (!itemList) {
             return <Spinner/>
         }
+        // const {data} = this.props;
 
-        const items = this.renderItems(itemList);
+        const items = renderItems(itemList);
 
 
         return (
@@ -52,4 +65,37 @@ export default class ItemList extends Component {
             </ul>
         );
     }
-}
+    export default ItemList;
+// }
+
+// ItemList.defaultProps = {
+//     onItemSelected: () => {}
+// }
+
+// const withData = (View) => {
+//     return class extends Component {
+//         state = {
+//             data: null
+//         }
+//         componentDidMount() {
+//             const {getData} = this.props;
+    
+//             getData()
+//                 .then( (data) => {
+//                     this.setState({
+//                         data
+//                     })
+//                 })
+//         }
+//         render() {
+//             const {data} = this.state;
+
+//         if (!data) {
+//             return <Spinner/>
+//         }
+
+//             return <View {...this.props} data={data} />
+//         }
+//     }
+// }
+// export default withData(ItemList);
