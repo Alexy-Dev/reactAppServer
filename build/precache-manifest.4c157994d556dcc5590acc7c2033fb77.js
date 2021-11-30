5fef20aad80983545da9fd5af9c87e2439f0997c
Oleksii Polonchuk <63574876+Alexy-Dev@users.noreply.github.com>
2021-11-30 11:46:03 +0200

    Add files via upload

---
+++ build/precache-manifest.4c157994d556dcc5590acc7c2033fb77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5adc7e4d3b5657dfc55f",
+    "url": "/static/css/main.8b92428c.chunk.css"
+  },
+  {
+    "revision": "5adc7e4d3b5657dfc55f",
+    "url": "/static/js/main.5adc7e4d.chunk.js"
+  },
+  {
+    "revision": "be53405b83e389a128f1",
+    "url": "/static/css/1.2e0f0beb.chunk.css"
+  },
+  {
+    "revision": "be53405b83e389a128f1",
+    "url": "/static/js/1.be53405b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a15bc4241ffc04bf1488597e670f051c",
+    "url": "/index.html"
+  }
+];
